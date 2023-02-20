d19b02e1be3299fd55736527c9e05e7649eff14b
CounterCookie <countercookio@gmail.com>
2023-02-20 00:01:04 -0800

    Painful filtering of inaccurate data.

--- get_classes.py
+++ get_classes.py
@@ -15,16 +15,32 @@ requestUrl = f"https://ssb9s.capilanou.ca:8443/StudentRegistrationSsb/ssb/search
 
 #data=json.loads(session.get(requestUrl).text)
 with open('spring22_200_data', 'r') as f:
-    data = json.loads(f.read())
+    data = json.loads(f.read())['data']
+'''
+ Valid classes are those without an empty "meetingsFaculty" and not a "None" building in the meetingTime dictionary.
+ "meetingsFaculty" is an array. some classes are both online and in person. or have lab/lecture
 
-@dataclass
-class Room:
-    roomname:str
-    @classmethod
-    def getClass(cls, data):
-        section = data['meetingsFaculty'][0]['meetingTime']
-        roomname = section['buildingDescription'] + section['room']
-        return Room(roomname)
 
-for i in data['data']:
-    print(Room.getClass(i))
+
+ For graphing the schedule for one day of the week, we need the room numbers and begin, end times.
+'''
+#@dataclass
+#class Room:
+#    roomname:str
+#    @classmethod
+#    def getClass(cls, data):
+#        section = data['meetingsFaculty'][0]['meetingTime']
+#        roomname = section['buildingDescription'] + section['room']
+#        return Room(roomname)
+c = 0
+for i in data:
+    print(c)
+    if (len(i['meetingsFaculty']) != 0) and (len([e for e in [d for d in i['meetingsFaculty']]]) != 0):
+        for f in [e for e in [d for d in i['meetingsFaculty']]]:
+            if (f['meetingTime']['building'] != None) and (f['meetingTime']['building'] != "ON") :
+                section = f['meetingTime']
+                roomname = (section['buildingDescription'] + section['room'])
+                print(roomname)
+
+
+    c += 1
