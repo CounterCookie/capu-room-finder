497129aa99cf00e5969b5f42d811f549e4916bcb
CounterCookie <countercookio@gmail.com>
2023-02-23 18:26:57 -0800

    Move the code to a notebook. Probably have merge them for it to look cleaner. Main code is ghetto.

--- get_classes.py
+++ get_classes.py
@@ -9,13 +9,13 @@ offset = 0 # Offset from the start. max requests is 200, increase by 200
 requestSize = 200 # Maximum
 
 
-#session.post('https://ssb9s.capilanou.ca:8443/StudentRegistrationSsb/ssb/term/search?mode=search', data=termData)
+session.post('https://ssb9s.capilanou.ca:8443/StudentRegistrationSsb/ssb/term/search?mode=search', data=termData)
 
 requestUrl = f"https://ssb9s.capilanou.ca:8443/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_term={termData['term']}&startDatepicker=&endDatepicker=&pageOffset={offset}&pageMaxSize={requestSize}&sortColumn=subjectDescription&sortDirection=asc"
 
-#data=json.loads(session.get(requestUrl).text)
-with open('spring22_200_data', 'r') as f:
-    data = json.loads(f.read())['data']
+data=json.loads(session.get(requestUrl).text)['data']
+#with open('spring22_200_data', 'r') as f:
+#    data = json.loads(f.read())['data']
 '''
  Valid classes are those without an empty "meetingsFaculty" and not a "None" building in the meetingTime dictionary.
  "meetingsFaculty" is an array. some classes are both online and in person. or have lab/lecture
@@ -23,24 +23,56 @@ with open('spring22_200_data', 'r') as f:
 
 
  For graphing the schedule for one day of the week, we need the room numbers and begin, end times.
+
+ For the class of dow. It needs to have the roomname, begintime, endtime.
+
+
+ CREATE A SET OF CLASS NAMES AND THEN GATHER THEM LIKE THAT
 '''
-#@dataclass
-#class Room:
-#    roomname:str
-#    @classmethod
-#    def getClass(cls, data):
-#        section = data['meetingsFaculty'][0]['meetingTime']
-#        roomname = section['buildingDescription'] + section['room']
-#        return Room(roomname)
-c = 0
-for i in data:
-    print(c)
-    if (len(i['meetingsFaculty']) != 0) and (len([e for e in [d for d in i['meetingsFaculty']]]) != 0):
-        for f in [e for e in [d for d in i['meetingsFaculty']]]:
-            if (f['meetingTime']['building'] != None) and (f['meetingTime']['building'] != "ON") :
-                section = f['meetingTime']
-                roomname = (section['buildingDescription'] + section['room'])
-                print(roomname)
+@dataclass
+class Monday:
+    roomname:str
+    begintime: int
+    endtime: int
+    # test
+    index: int
+    # test
 
+    @classmethod
+    def room(cls, data, num):
+        roomname = data['buildingDescription'] + data['room']
+        begintime = data['beginTime']
+        endtime = data['endTime']
+        #test
+        index = num
+        #test
 
-    c += 1
+        return Monday(roomname, begintime, endtime, index)
+
+def roomSearch(data):
+    c = 0
+    monday = []
+    for i in data:
+    #    print(c)
+        if (len(i['meetingsFaculty']) != 0) and (len([e for e in [d for d in i['meetingsFaculty']]]) != 0):
+            for f in i['meetingsFaculty']:
+                if (f['meetingTime']['building'] != None) and (f['meetingTime']['building'] != "ON") :
+                    section = f['meetingTime']
+                    roomname = (section['buildingDescription'] + section['room'])
+    #                print(roomname)
+                    if section['monday'] == True:
+                        monday.append(Monday.room(section, c))
+    
+    
+    
+        c += 1
+    print(monday)
+    return monday
+    #for i in monday:
+    #    print(i.roomname, i.begintime, i.endtime, i.index)
+roomSearch(data)
+#for i in range(15):
+#    requestUrl = f"https://ssb9s.capilanou.ca:8443/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_term={termData['term']}&startDatepicker=&endDatepicker=&pageOffset={offset}&pageMaxSize={requestSize}&sortColumn=subjectDescription&sortDirection=asc"
+#    data=json.loads(session.get(requestUrl).text)['data']
+#    roomSearch(data)
+#    offset += 200
